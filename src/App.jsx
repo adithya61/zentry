fcf016d23313852e4489429dcd202b97d77fc020
adithya61 <zonic662@gmail.com>
2024-12-15 18:16:29 +0530

    resolve deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Story from "./components/Story";
 
 const App = () => {
   return (
     <main className=" relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar />
+      <Navbar />
       <Hero />
       <About />
       <Features />
